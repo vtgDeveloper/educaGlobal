c9478e1def520df08f4d8ee859122eb8aea16513
vtgDeveloper <vinctg2003@gmail.com>
2022-05-16 09:36:49 -0500

    Main frame

--- index.js
+++ index.js
@@ -6,24 +6,3 @@ const appear = ()=>{
 }
 setTimeout(appear, 5000);
 
-
-/*var sound = new Audio('https://www.youtube.com/watch?v=A9Sn7t9rMYU');
-sound.play();
-*/
-
-/*audio.addEventListener("animationiteration", ()=>{
-    sound.play();
-})
-
-
-const stopAudio = ()=>{
-    sound.pause();
-}
-
-setTimeout(stopAudio, 5000);
-*/
-
-
-
-
-
