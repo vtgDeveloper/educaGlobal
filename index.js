28cb4914c4754bada8c4b0aef980fba63d76eac6
vtgDeveloper <vinctg2003@gmail.com>
2022-05-15 23:01:47 -0500

    Registro usuarios

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const contenedor = document.querySelector(".container");
+const audio = document.querySelector(".container");
 
 const appear = ()=>{
     contenedor.style.display = "none";
 }
-
 setTimeout(appear, 5000);
+
+
+/*var sound = new Audio('https://www.youtube.com/watch?v=A9Sn7t9rMYU');
+sound.play();
+*/
+
+/*audio.addEventListener("animationiteration", ()=>{
+    sound.play();
+})
+
+
+const stopAudio = ()=>{
+    sound.pause();
+}
+
+setTimeout(stopAudio, 5000);
+*/
+
+
+
 
 
